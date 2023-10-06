0055d814389cd26d68997a01dc391b2eb4342810
DorukEmre <doruk.emre@outlook.com>
2023-10-06 15:06:59 +0100

    Added use of NLM resources statement

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,6 +5,12 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <p>Hello</p>
+      <p>
+        "This product uses publicly available data from the U.S. National
+        Library of Medicine (NLM), National Institutes of Health, Department of
+        Health and Human Services; NLM is not responsible for the product and
+        does not endorse or recommend this or any other product."
+      </p>
     </main>
   )
 }
